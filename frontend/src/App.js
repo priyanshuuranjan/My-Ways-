a9f7841eadced7442938220ef4d6a5d18ab5b1df
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-19 16:54:42 +0530

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login"
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Routes>
       <Route exact path="/" element={<Login />} />
+      <Route exact path="/" element={<Dashboard />} />
       </Routes>
     </div>
   );
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Alert } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import styles from "./Login.module.css"; // Import the CSS module
 
-export default function Login() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.loginBox}>
+        <h2 className={styles.heading}>Login</h2>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              placeholder="Email address"
+              className={styles.inputField}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              className={styles.inputField}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <div className={styles.btnContainer}>
+            <Button className={styles.loginBtn} type="submit">
+              Log In
+            </Button>
+          </div>
+        </Form>
+        <div className={styles.orText}>OR</div>
+        <div className={styles.signupBox}>
+          <p className={styles.signupText}>
+            Don't have an account?{" "}
+            <Link to="/signup" className={styles.signupLink}>
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
