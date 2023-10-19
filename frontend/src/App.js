f84b99d7ccd2fa46c5e7b2b78838ccfe18c0ddbf
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-19 17:23:57 +0530

    close

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import Login from "./components/Login"
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route exact path="/" element={<Login />} />
-      <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Dashboard />} />
       </Routes>
     </div>
   );
