a3090194e33f57831a98d91db1fbe02416fe1afe
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-20 00:51:58 +0530

    All done final commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./Dashboard.css";
 
 const Card = () => {
-    return (
-      <div className="card">
+  return (
+    <div className="card">
       <div className="left-section">
         <div className="chat-room-header">
           <h3>Chat Room</h3>
           <i className="camera-icon"></i>
         </div>
         <div className="message-header">
-        <input type="text" placeholder="Write a message" />
+          <input type="text" placeholder="Write a message" />
         </div>
       </div>
       <div className="right-section">
-        <div className="task-board">
+        <div className="">
+          <h4>Task Board</h4>
+          <div className="task-board"></div>
           <div className="task-category">To Do</div>
           <div className="task-category">In Progress</div>
           <div className="task-category">Done</div>
