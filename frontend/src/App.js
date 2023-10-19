e1030b8be71f9cf95eb0eb4599a8041cd5666f0d
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-10-19 16:32:10 +0530

    setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login"
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+      <Route exact path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
